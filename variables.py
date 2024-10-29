33769c28aa915d6a79d93bf39da923879ca23e3c
Zireael <145209614+Tamerlan021@users.noreply.github.com>
2024-10-29 13:22:14 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+all_tasks = 12
+time_spent = 1.5
+name_course = 'Python'
+average_run_time = time_spent / all_tasks
+print('Course:',name_course, "All_tasks:", all_tasks, 'Time_spent:', time_spent, 'Average_run_time:', average_run_time)
